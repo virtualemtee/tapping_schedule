4ea5d488926a487773e6818c687d672f2fa23686
virtualemtee <virtualmsofficeassistant@gmail.com>
2024-10-31 14:54:12 +0000

    Update olderapp.py

--- olderapp.py
+++ olderapp.py
@@ -275,42 +275,42 @@ if uploaded_file is not None:
         for pairing in closest_improving_data:
             summary_data.append({
               #  "Type": "Poor Grades Bettered",
-                "Main_Cell": pairing["Poor_Cell"],
-                "Paired_Cell": pairing["Improving_Cell"],
-                "Resultant_Grade": pairing["Resultant_Grade"]
+                "Main_Cell": pairing["Cell"],
+                "Paired_Cell": pairing["Pair"],
+                "Resultant_Grade": pairing["Grade"]
             })
         
         for pairing in pairable_grades_data:
             summary_data.append({
               #  "Type": "Non-Improved Grades",
-                "Main_Cell": pairing["Base_Cell"],
-                "Paired_Cell": pairing["Pairable_Cell"],
-                "Resultant_Grade": pairing["Resultant_Grade"]
+                "Main_Cell": pairing["Cell"],
+                "Paired_Cell": pairing["Pair"],
+                "Resultant_Grade": pairing["Grade"]
             })
         
         for pairing in acceptable_pairings_data:
             summary_data.append({
            #     "Type": "Acceptable Grades",
-                "Main_Cell": pairing["Acceptable_Cell"],
-                "Paired_Cell": pairing["Pairing_Cell"],
-                "Resultant_Grade": pairing["Resultant_Grade"]
+                "Main_Cell": pairing["Cell"],
+                "Paired_Cell": pairing["Pair"],
+                "Resultant_Grade": pairing["Grade"]
             })
         
         for pairing in additional_pairings:
             summary_data.append({
            #     "Type": "Acceptable & Non-Improved",
-                "Main_Cell": pairing["Acceptable_Cell"],
-                "Paired_Cell": pairing["Non_Improving_Cell"],
-                "Resultant_Grade": pairing["Resultant_Grade"]
+                "Main_Cell": pairing["Cell"],
+                "Paired_Cell": pairing["Pair"],
+                "Resultant_Grade": pairing["Grade"]
             })
         
         # Append unpaired cells to the summary as well
         for cell in remaining_cells:
             summary_data.append({
           #      "Type": "Remaining Cells",
-                "Main_Cell": cell["Remaining_Cell"],
+                "Main_Cell": cell["Standalone"],
                 "Paired_Cell": None,
-                "Resultant_Grade": cell["Individual_Grade"]
+                "Resultant_Grade": cell["Grade"]
             })
         
         # Convert the summary to a DataFrame for display
